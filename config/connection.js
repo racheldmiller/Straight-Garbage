fa48fd3e7b9cdf3a5f3e87fa7b1d5a045852b911
racheldmiller <rachelmiller415@gmail.com>
2019-06-22 13:29:38 -0700

    update connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,25 @@
 // handles the connection/login info for the database, holds method for starting connection
 
+// Set up MySQL connection
 const mysql = require("mysql");
 
 let connection = mysql.createConnection({
   host: "localhost",
+  port: 8080,
   user: "root",
   password: "password",
-  database: "something_db"
-  // need to rename database to match whatever we name our actual database
+  database: "recycleThis_db"
+  // ** this is a temporary database name **
 });
 
-connection.connect();
+// Make the connection
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("conneected as id " + connection.threadId);
+});
 
+// Export connection for our ORM to use.
 module.exports = connection;
