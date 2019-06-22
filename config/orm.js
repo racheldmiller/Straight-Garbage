60e193f86ef961d5e519fc29ac76e098f8f89ed7
Randall Braden <randalllb@Randalls-MacBook-Pro-2.local>
2019-06-22 11:31:02 -0700

    saved changes to garbage-controller

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,8 @@
 // Import MySQL connection.
 // Object for all our SQL statement functions. (CRU: create, read, update)
+var orm ={
+
+};
+
+// Export the orm object for the model (garbage.js).
+module.exports = orm;
--- controllers/garbageControllers.js
+++ controllers/garbageControllers.js
@@ -1,4 +1,30 @@
 // declares all routes and what logic should run if URL matches (GET, POST, PUT, DELETE), 
 //       touches model, figures out what view to render
 // Export routes for server.js to use.
+
+
+// ===================================
+//  DEPENDENCIES & CONFIGS
+// ===================================
+
+var express = require("express");
+
+var router = express.Router();
+
+var garbage = require("../models/garbage.js");
+
+// ===================================
+//  ROUTES
+// ===================================
+
+router.get('/', function (req, res) {
+    //res.JSON like console.log to display a message to the screen that the page works
+    res.json("Route for / works")
+})
+
+
+
+// ===================================
+//  EXPORT
+// ===================================
 module.exports = router;
