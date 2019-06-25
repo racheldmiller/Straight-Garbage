96f2ded4e6c5b7bce9423b71fbd3c06ad3f336c3
racheldmiller <rachelmiller415@gmail.com>
2019-06-24 20:40:24 -0700

    schema and updated api routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,13 @@
 -- creates database, uses database, sets up table to accept values
+
+CREATE DATABASE recycleThis_db;
+USE recycleThis_db;
+
+CREATE TABLE materials
+(
+	id int NOT NULL AUTO_INCREMENT,
+	type VARCHAR NOT NULL,
+    image VARCHAR,
+	material_id INT NOT NULL, 
+	PRIMARY KEY (id)
+);
