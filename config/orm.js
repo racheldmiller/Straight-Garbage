b74e1635b0b27c0d9d754e5d82b14f505ea8e818
Gunnerson <brandongunnerson@gmail.com>
2019-06-24 18:10:11 -0700

    front-end rough draft

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,5 @@
 // Import MySQL connection.
 // Object for all our SQL statement functions. (CRU: create, read, update)
+
+// Import MySQL connection
+var connection = require("../config/connection.js");
---
+++ config/test.js
@@ -0,0 +1,14 @@
+// just seeing what information we get from the API key
+
+var queryURL =
+  "http://api.earth911.com/" +
+  +"earth911.getMaterials?api_key=" +
+  "EARTH911";
+
+// make the AJAX call
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
