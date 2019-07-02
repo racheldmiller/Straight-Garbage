d35ebca1fa23dcba37459270affae9d076a5cbfd
unknown <pkell@DESKTOP-ED2GH22.(none)>
2019-07-01 21:03:26 -0700

    JS and CSS files

---
+++ js/init.js
@@ -0,0 +1,21 @@
+// This is jQuery code used when the browser window viewport is in mobile view.
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// This is the action to use when using the dropdown option.
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
+
+// Or with jQuery
+
+$('.dropdown-trigger').dropdown();
+
