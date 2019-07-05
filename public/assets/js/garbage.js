afab6e71f232201782dcf5e089847d8540be61a7
Randall Braden <randalllb@Randalls-MacBook-Pro-2.local>
2019-07-04 23:15:44 -0700

    working ajax calls, but CORS policy is preventing exceution

--- public/assets/js/garbage.js
+++ public/assets/js/garbage.js
@@ -1,76 +1,130 @@
-//Google maps documentation
-// var map;
-// function initMap(lat, lng) {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {
-//         //pass in the variables here for lat lng
-//         lat: -34.397, 
-//         lng: 150.644},
-//     zoom: 8
-//   });
-// }
+// jQuery ready func
+$(document).ready(function () {
+    // //Google maps documentation
+    var map;
+    function initMap(lat, lng) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                //pass in the variables here for lat lng
+                lat: lat,
+                lng: lng
+            },
+            zoom: 8
+        });
+    }
 
-require('dotenv').config();
-console.log(process.env);
-var axios = require("axios");
+    // //USING JQUERY TO GRAB FORM INPUT FROM  disposeLocForm.handlebars
 
-//USING JQUERY TO GRAB FORM INPUT FROM  disposeLocForm.handlebars
+    $("#submit").on("click", function (event) {
+        // prevent page refresh
+        event.preventDefault();
+        console.log("submit was clicked")
 
-$("button").on("click", function(event) {
-    event.preventDefault();
+        //capture form inputs from disposeLocForm
+        var street = $("#Address").val();
+        var city = $("#City").val();
+        var state = $("#State").val();
 
-    var street = $("#Address").val();
-    var city = $("#City").val();
-    var state= $("#State").val();
+        console.log("ln 40", street, city, state)
 
-    var address = {
-        street: street,
-        city: city,
-        state: state
-    };
+        // if any fields are empty, return a error message
+        $('input').each(function () {
+            if (!$(this).val()) {
+                alert("Please review missing fields and try again.");
+                return false;
+            }
+        });
 
-    console.log("garbage.js, ln 16 address: ", address);
+        //     var address = {
+        //         street: street,
+        //         city: city,
+        //         state: state
+        //     };
 
-    //send to server
-    $.post("/disposallocationresults", address, function(response) {
-        console.log("garbage.js, ln 20: ", response)
-    })
+        //     // test var
+        // var street = "160 Spear Street";
+        // var city = "San Francisco";
+        // var state = "CA";
+        // var lat = 33.67;
+        // var lng = -112.84;
+        //     //.end test var
 
-    function getCoord(){
-    axios
-    console.log("https://maps.googleapis.com/maps/api/geocode/json?address=" + street + ",+" + city + ",+ " + state + "&key=" + process.env.GOOGLE)
-        .get(
-            // input from user form
-            "https://maps.googleapis.com/maps/api/geocode/json?address=" 
-            + street 
-            + ",+" 
-            + city 
-            + ",+ " 
-            + state 
-            + "&key=" 
-            + process.env.GOOGLE
-            //  Template:            
-            //  "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=" +
-            //   process.env.GOOGLE
-        )
-        .then(function(response) {
+        var cnctStreet = street.replace(/\s+/g, '+');
+        var cnctCity = city.replace(/\s+/g, '+');
+        var cnctState = state.replace(/\s+/g, '+');
+
+        var lat;
+        var lng;
+
+        var max_distance = 25;
+        //     console.log("garbage.js, ln 16 address: ", address);
+
+        //     //send to server
+        //     $.post("/disposallocationresults", address, function(response) {
+        //         console.log("garbage.js, ln 20: ", response)
+        //     })
+
+        var queryURL1 =
+            "https://maps.googleapis.com/maps/api/geocode/json?address="
+            + cnctStreet
+            + ",+"
+            + cnctCity
+            + ",+ "
+            + cnctState
+            + "&key=AIzaSyAu1DCEzzMt3HyoNm90Myqn6cjWdevfOdc";
+
+        // var query2 = 
+        // "http://api.earth911.com/earth911.searchLocations?latitude=" +
+        // lat +
+        // "&longitude=" +
+        // lng +
+        // "&max_distance=" +
+        // max_distance +
+        // "&api_key=6ee9222b20473f36";
+
+
+        $.ajax({
+            url: queryURL1,
+            method: "GET",
+            //to pass CORS request
+            contentType: 'text/plain',
+            xhrFields: {
+                withCredentials: false
+            }
+        }).then(function (response) {
+            //   console.log("ln 93", response)
             console.log("************");
-            var lat = response.data.results[0].geometry.location.lat;
+            lat = response.results[0].geometry.location.lat;
             console.log("This is the lat: ", lat);
-            var lng = response.data.results[0].geometry.location.lng;
+            lng = response.results[0].geometry.location.lng;
             console.log("This is the longitude: ", lng);
             console.log("************");
-        })
-        // .then(
-        //     .get()
-        //     //  another api call to google maps
-        //     //  make a new map 
-        //     //  'center' at two coordinates from geocode api call        
-        //     )
-    }
-})
+        }).then(
+            $.ajax({
+                url:
+                    "http://api.earth911.com/earth911.searchLocations?latitude=" +
+                    lat +
+                    "&longitude=" +
+                    lng +
+                    "&max_distance=" +
+                    max_distance +
+                    "&api_key=6ee9222b20473f36",
+                method: "GET"
+            }).then(function (response) {
+                console.log(response.data.result);
+                var description = [];
+                var distance = [];
+                var location_id = [];
+                for (var i = 0; i < response.data.result.length; i++) {
+                    console.log(response.data.result[i]);
+                }
+            })
+        )
+    })
+});
 
-getCoord();
+
+
 
 
 // a handle click function that plugs in the whereButton form into the getcoord()
