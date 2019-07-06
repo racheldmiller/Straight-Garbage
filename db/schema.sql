8d0113e9df26c4b39c678aa272c6ef3be827d4f4
racheldmiller <rachelmiller415@gmail.com>
2019-07-06 09:10:57 -0700

    updated

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,6 @@ USE recyclethis_db;
 
 CREATE TABLE materials (
 	id INT(11) NOT NULL AUTO_INCREMENT,
-	nums INT(11) NOT NULL,
 	type VARCHAR(250) NOT NULL DEFAULT "unknown",
     description VARCHAR(500) NOT NULL, 
     image VARCHAR(250) DEFAULT NULL, 
