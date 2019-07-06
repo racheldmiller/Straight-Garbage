28e1b989ab0e444d4f0ebe1bcf15ac9306f2a356
racheldmiller <rachelmiller415@gmail.com>
2019-07-06 09:14:00 -0700

    commenting out files we dont need atm

--- config/orm.js
+++ config/orm.js
@@ -1,83 +1,83 @@
 // Import MySQL connection.
 
-var connection = require("./connection");
+// var connection = require("./connection");
 
-// =========================
-// BOILER PLATES FOR ?'s and SQL CONVERSIONS
-// =========================
+// // =========================
+// // BOILER PLATES FOR ?'s and SQL CONVERSIONS
+// // =========================
 
-// Boiler plate that calculates the use of question marks
-function printQuestionMarks(num) {
-  var arr = [];
+// // Boiler plate that calculates the use of question marks
+// function printQuestionMarks(num) {
+//   var arr = [];
 
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
+//   for (var i = 0; i < num; i++) {
+//     arr.push("?");
+//   }
 
-  return arr.toString();
-}
+//   return arr.toString();
+// }
 
-// Boiler plate that converts object key/val pairs to sql syntax
-function objToSql(ob) {
-  var arr = [];
-  for (var key in ob) {
-    var value = ob[key];
+// // Boiler plate that converts object key/val pairs to sql syntax
+// function objToSql(ob) {
+//   var arr = [];
+//   for (var key in ob) {
+//     var value = ob[key];
 
-    if (Object.hasOwnProperty.call(ob, key)) {
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
+//     if (Object.hasOwnProperty.call(ob, key)) {
+//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
+//         value = "'" + value + "'";
+//       }
 
-      arr.push(key + "=" + value);
-    }
-  }
+//       arr.push(key + "=" + value);
+//     }
+//   }
 
-  return arr.toString();
-}
+//   return arr.toString();
+// }
 
-// =========================
-// ORM
-// =========================
-// Object for all our SQL statement functions. (CRU: create, read, update)
-var orm = {
-  selectAll: function(materials, callback) {
-    var queryString = "SELECT * FROM " + materials + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      callback(result);
-    });
-  },
+// // =========================
+// // ORM
+// // =========================
+// // Object for all our SQL statement functions. (CRU: create, read, update)
+// var orm = {
+//   selectAll: function(materials, callback) {
+//     var queryString = "SELECT * FROM " + materials + ";";
+//     connection.query(queryString, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
+//       callback(result);
+//     });
+//   },
 
-  create: function(table, cols, vals, callback) {
-    var queryString = "INSERT INTO " + table;
+//   create: function(table, cols, vals, callback) {
+//     var queryString = "INSERT INTO " + table;
 
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+//     queryString += " (";
+//     queryString += cols.toString();
+//     queryString += ") ";
+//     queryString += "VALUES (";
+//     queryString += printQuestionMarks(vals.length);
+//     queryString += ") ";
 
-    console.log(queryString);
+//     console.log(queryString);
 
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      callback(result);
-    });
-  },
+//     connection.query(queryString, vals, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
+//       callback(result);
+//     });
+//   },
 
-  find: function(tableInput, id, cb) {
-    var queryString = "SELECT * FROM " + tableInput;
+//   find: function(tableInput, id, cb) {
+//     var queryString = "SELECT * FROM " + tableInput;
 
-    queryString += "WHERE";
-    queryString += "?";
-    console.log(queryString);
-  }
-};
+//     queryString += "WHERE";
+//     queryString += "?";
+//     console.log(queryString);
+//   }
+// };
 
-// Export the orm object for the model (garbage.js).
-module.exports = orm;
+// // Export the orm object for the model (garbage.js).
+// module.exports = orm;
